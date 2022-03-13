50f748381c47234852369b35e4af23a64a31cd2b
NotDSF <67711164+NotDSF@users.noreply.github.com>
2022-03-13 22:07:35 +0000

    Added CLI commands (Make sure to use the latest version @ https://github.com/NotDSF/HttpSpy)

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 --[[
-    HttpSpy v1.0.7
+    HttpSpy v1.0.8
 ]]
 
 assert(syn, "Unsupported exploit");
 
 local options = ({...})[1] or {};
-local version = "v1.0.7";
+local version = "v1.0.8";
 local logname = string.format("%s-log.txt", string.gsub(syn.crypt.base64.encode(syn.crypt.random(5)), "%p", ""));
 
 if not isfile(logname) then writefile(logname, string.format("Http Logs from %s\n\n", os.date("%d/%m/%y"))) end;
@@ -16,6 +16,11 @@ local format = string.format;
 local gsub = string.gsub;
 local match = string.match;
 local append = appendfile;
+local Unpack = unpack;
+local Type = type;
+local Rawget = rawget;
+local blocked = {};
+local enabled = true;
 local methods = {
     HttpGet = true,
     HttpGetAsync = true,
@@ -43,8 +48,17 @@ __namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
 end));
 
 __request = hookfunction(syn.request, newcclosure(function(req) 
+    if not enabled then
+        return __request(req);
+    end;
+
     local BE = Instance.new("BindableEvent");
     coroutine.wrap(function() 
+        if Type(req) == "table" and Rawget(req, "Url") and blocked[req.Url] then
+            printf("syn.request(%s) -- blocked url\n\n", Serializer.Serialize(req));
+            return BE.Fire(BE, {});
+        end;
+
         local ResponseData = __request(req);
         local BackupData, IsJSON = {};
 
@@ -78,6 +92,10 @@ end));
 if options.WebsocketSpy then
     local id = 1;
     __websocket = hookfunction(syn.websocket.connect, function(url) 
+        if not enabled then
+            return __websocket(url);
+        end;
+
         local BE = Instance.new("BindableEvent");
         coroutine.wrap(function() 
             local WebsocketId = "WS_" .. id;
@@ -126,4 +144,57 @@ for method in pairs(methods) do
     end));
 end;
 
-pconsole(format("HttpSpy %s\nCreated by https://github.com/NotDSF\nChange Logs:\n\t%s\nLogs are automatically being saved to: %s\n\n", version, RecentCommit, logname))
+pconsole(format("HttpSpy %s (Creator: https://github.com/NotDSF)\nMake sure you are using the loadstring for live updates @ https://github.com/NotDSF/HttpSpy\nChange Logs:\n\t%s\nLogs are automatically being saved to: %s\nType \"cmds\" to view a list of commands\n\n", version, RecentCommit, logname));
+
+local Commands = {};
+local function RegisterCommand(name, argsr, func) 
+    Commands[name] = {
+        ArgsRequired = argsr,
+        Command = func
+    }
+end;
+
+RegisterCommand("cmds", 0, function() pconsole("List of commands:\n\tblock[=url]: will block any request with the specified url (the request will still be shown on the spy)\n\tunblock[=url]: will unblock the specified url\n\tenable: will enable the spy\n\tdisable: will disable the spy\n\tcls: will clear the console\n\n"); end);
+RegisterCommand("enable", 0, function() enabled = true; pconsole("The spy is now enabled!\n\n"); end);
+RegisterCommand("disable", 0, function() enabled = false; pconsole("The spy is now disabled!\n\n") ;end);
+RegisterCommand("cls", 0, rconsoleclear);
+
+RegisterCommand("block", 1, function(url) 
+    blocked[url] = true;
+    pconsole(format("Blocked url: '%s'\n\n", url));
+end);
+
+RegisterCommand("unblock", 1, function(url) 
+    if not blocked[url] then
+        pconsole(format("This url isn't blocked\n\n"));
+    end;
+    blocked[url] = false;
+    pconsole(format("Unblocked url: '%s'\n\n", url));
+end);
+
+while true do 
+    local Input, Args, Command = rconsoleinput(), {};
+
+    for i in string.gmatch(Input, "[^%s]+") do
+        if not Command then 
+            Command = i;
+            continue;
+        end;
+        Args[#Args+1] = i;
+    end;
+
+    if not Command then continue end;
+
+    local CommandInfo = Commands[Command];
+    if not CommandInfo then
+        pconsole(format("'%s' is not a command, type \"cmds\" to view the commands\n\n", Command));
+        continue;
+    end;
+
+    if CommandInfo.ArgsRequired > #Args then
+        pconsole(format("'%s' requires %d arguments but you provided %d\n\n", Command, CommandInfo.ArgsRequired, #Args));
+        continue;
+    end;
+
+    CommandInfo.Command(Unpack(Args));
+end;
