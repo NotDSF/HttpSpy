17c3a500ec360f996a7f98e0f50034b991c30a1f
dsf <67711164+NotDSF@users.noreply.github.com>
2022-09-04 21:38:13 +0100

    Added more API functions, to use the API you now need to enable the "API" setting.

--- init.lua
+++ init.lua
@@ -1,15 +1,14 @@
 --[[
-    HttpSpy v1.1.2
+    HttpSpy v1.1.3
 ]]
 
 assert(syn or http, "Unsupport exploit (should support syn.request or http.request)");
 
-local options = ({...})[1] or { AutoDecode = true, Highlighting = true, SaveLogs = true, CLICommands = true, ShowResponse = true, BlockedURLs = {} };
-local version = "v1.1.2";
-local logname = string.format("HttpSpy/%d-%s-log.txt", game.PlaceId, os.date("%d_%m_%y"));
+local options = ({...})[1] or { AutoDecode = true, Highlighting = true, SaveLogs = true, CLICommands = true, ShowResponse = true, BlockedURLs = {}, API = true };
+local version = "v1.1.3";
+local logname = string.format("%d-%s-log.txt", game.PlaceId, os.date("%d_%m_%y"));
 
 if options.SaveLogs then
-    if not isfolder("HttpSpy") then makefolder("HttpSpy") end; -- OmG isfolder("HttpSpy") DeTeCtIoN! (cough stan) 
     writefile(logname, string.format("Http Logs from %s\n\n", os.date("%d/%m/%y"))) 
 end;
 
@@ -28,11 +27,13 @@ local cyield = clonef(coroutine.yield);
 local Pcall = clonef(pcall);
 local Pairs = clonef(pairs);
 local Error = clonef(error);
+local getnamecallmethod = clonef(getnamecallmethod);
 local blocked = options.BlockedURLs;
 local enabled = true;
 local reqfunc = (syn or http).request;
 local libtype = syn and "syn" or "http";
 local hooked = {};
+local proxied = {};
 local methods = {
     HttpGet = not syn,
     HttpGetAsync = not syn,
@@ -43,6 +44,9 @@ local methods = {
 
 Serializer.UpdateConfig({ highlighting = options.Highlighting });
 
+local RecentCommit = game.HttpService:JSONDecode(game:HttpGet("https://api.github.com/repos/NotDSF/HttpSpy/commits?per_page=1&path=init.lua"))[1].commit.message;
+local OnRequest = Instance.new("BindableEvent");
+
 local function printf(...) 
     if options.SaveLogs then
         append(logname, gsub(format(...), "%\27%[%d+m", ""));
@@ -51,7 +55,7 @@ local function printf(...)
 end;
 
 local function ConstantScan(constant)
-    for i,v in pairs(getgc(true)) do
+    for i,v in Pairs(getgc(true)) do
         if type(v) == "function" and islclosure(v) and getfenv(v).script == getfenv(saveinstance).script and table.find(debug.getconstants(v), constant) then
             return v;
         end;
@@ -103,6 +107,15 @@ __request = hookfunction(reqfunc, newcclosure(function(req)
             return cresume(t, {});
         end;
 
+        if RequestData.Url then
+            local Host = string.match(RequestData.Url, "https?://(%w+.%w+)/");
+            if Host and proxied[Host] then
+                RequestData.Url = gsub(RequestData.Url, Host, proxied[Host], 1);
+            end; 
+        end;
+
+        OnRequest:Fire(RequestData);
+
         local ok, ResponseData = Pcall(__request, RequestData); -- I know of a detection with this
         if not ok then
             Error(ResponseData, 0);
@@ -131,7 +144,7 @@ if request then
     replaceclosure(request, reqfunc);
 end;
 
-if syn then
+if syn and syn.websocket then
     local WsConnect, WsBackup = debug.getupvalue(syn.websocket.connect, 1);
     WsBackup = hookfunction(WsConnect, function(...) 
         printf("syn.websocket.connect(%s)\n\n", Serializer.FormatArguments(...));
@@ -139,10 +152,8 @@ if syn then
     end);
 end;
 
-local RecentCommit = game.HttpService:JSONDecode(game.HttpGet(game, "https://api.github.com/repos/NotDSF/HttpSpy/commits?per_page=1&path=init.lua"))[1].commit.message;
-
 -- I already know this will make some people mad :troll:
-if syn then
+if syn and syn.websocket then
     local HttpGet;
     HttpGet = hookfunction(getupvalue(ConstantScan("ZeZLm2hpvGJrD6OP8A3aEszPNEw8OxGb"), 2), function(self, ...) 
         printf("game.HttpGet(game, %s)\n\n", Serializer.FormatArguments(...));
@@ -166,13 +177,38 @@ for method, enabled in Pairs(methods) do
     end;
 end;
 
-pconsole(format("HttpSpy %s (Creator: https://github.com/NotDSF)\nMake sure you are using the loadstring for live updates @ https://github.com/NotDSF/HttpSpy\nChange Logs:\n\t%s\nLogs are automatically being saved to: \27[32m%s\27[0m\n\n", version, RecentCommit, options.SaveLogs and logname or "(You aren't saving logs, enable SaveLogs if you want to save logs)"));
+if not debug.info(2, "f") then
+    pconsole("You are running an outdated version, please use the loadstring at https://github.com/NotDSF/HttpSpy\n");
+end;
+
+pconsole(format("HttpSpy %s (Creator: https://github.com/NotDSF)\nChange Logs:\n\t%s\nLogs are automatically being saved to: \27[32m%s\27[0m\n\n", version, RecentCommit, options.SaveLogs and logname or "(You aren't saving logs, enable SaveLogs if you want to save logs)"));
+
+if not options.API then return end;
 
 local API = {};
+API.OnRequest = OnRequest.Event;
 
 function API:HookSynRequest(url, hook) 
     hooked[url] = hook;
 end;
+
+function API:ProxyHost(host, proxy) 
+    proxied[host] = proxy;
+end;
+
+function API:RemoveProxy(host) 
+    if not proxied[host] then
+        error("host isn't proxied", 0);
+    end;
+    proxied[host] = nil;
+end;
+
+function API:UnHookSynRequest(url) 
+    if not hooked[url] then
+        error("url isn't hooked", 0);
+    end;
+    hooked[url] = nil;
+end
 
 function API:BlockUrl(url) 
     blocked[url] = true;
